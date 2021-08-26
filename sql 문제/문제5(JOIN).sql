a9445c5362adef18b30854a209dc67c53bbcf05c
tnfla1226 <tnfla1226@naver.com>
2021-08-26 11:52:06 +0900

    [21.08.26] #SQL JOIN 기초2

---
+++ sql 문제/문제5(JOIN).sql
@@ -0,0 +1,46 @@
+-- # 실습 문제
+-- 1. employees, departments테이블을 INNER JOIN해서
+--    first_name, department_id, department_name을 조회하세요.
+SELECT COUNT(*) FROM employees;
+SELECT employee_id, department_id FROM employees;
+
+SELECT E.first_name, E.department_id, D.department_name
+FROM employees E, departments D
+WHERE E.department_id = D.department_id;
+
+SELECT E.first_name, E.department_id, D.department_name
+FROM employees E
+JOIN departments D
+ON E.department_id = D.department_id;
+
+SELECT E.first_name, department_id, D.department_name
+FROM employees E
+JOIN departments D
+USING (department_id);
+
+-- 2. employees, departments테이블을 NATURAL JOIN해서
+--    first_name, department_id, department_name을 조회하세요.
+
+SELECT E.first_name, department_id, D.department_name
+FROM employees E 
+NATURAL JOIN departments D;
+
+SELECT E.first_name, department_id, D.department_name
+FROM employees E 
+JOIN departments D 
+USING(department_id, manager_id);
+
+SELECT E.first_name, E.department_id, D.department_name
+FROM employees E
+JOIN departments D
+ON E.department_id = D.department_id
+    AND E.manager_id = D.manager_id;
+
+-- 3. employees, departments테이블을 JOIN ON을 사용해서
+--    first_name, department_id, department_name을 조회하세요.
+
+SELECT E.first_name, E.department_id, D.department_name
+FROM employees E
+JOIN departments D
+ON E.department_id = D.department_id;
+
